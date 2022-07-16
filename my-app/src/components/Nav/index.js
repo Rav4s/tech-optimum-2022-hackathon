78f07eb59d4a54867de1be0b300fda4fd111c0e4
JadonLee8 <jadonlee888@gmail.com>
2022-07-16 15:20:56 -0500

    cleaned up

--- my-app/src/components/Nav/index.js
+++ my-app/src/components/Nav/index.js
@@ -41,32 +41,6 @@ function NavBar() {
                 Signed in as <span>{name}</span>
             </div>
         </div>
-        //bootstrap not working, going to write own navbar
-        // <Container className='Navbar-container'>
-        // <Navbar bg="light" className='Navbar'>
-        // <Container>
-        //     <Navbar.Brand href="#home" className='brand'>React-Bootstrap</Navbar.Brand>
-        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        //     <Navbar.Collapse id="basic-navbar-nav">
-        //     <Nav className="me-auto">
-        //         <Nav.Link href="#home">Home</Nav.Link>
-        //         <Nav.Link href="#link">Link</Nav.Link>
-        //         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        //         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        //         <NavDropdown.Item href="#action/3.2">
-        //             Another action
-        //         </NavDropdown.Item>
-        //         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        //         <NavDropdown.Divider />
-        //         <NavDropdown.Item href="#action/3.4">
-        //             Separated link
-        //         </NavDropdown.Item>
-        //         </NavDropdown>
-        //     </Nav>
-        //     </Navbar.Collapse>
-        // </Container>
-        // </Navbar>
-        // </Container>
     )
 }
 
