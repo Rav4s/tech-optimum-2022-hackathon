222b98968ee81042599863bf6088772fd6af9b78
JadonLee8 <jadonlee888@gmail.com>
2022-07-16 18:46:31 -0500

    text box created

--- my-app/src/components/CountryInfoPage.js
+++ my-app/src/components/CountryInfoPage.js
@@ -2,14 +2,15 @@ import { useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 
 function CountryInfoPage(){
+    const [countryName, setCountryName] = useState("");
     return (
         <>
             <div className="input area">
             <input
                 type="text"
                 className="login__textBox"
-                //value={email}
-                //onChange={(e) => setEmail(e.target.value)}
+                value={countryName}
+                onChange={(e) => setCountryName(e.target.value)}
                 placeholder="Country Name"
             />
             </div>
