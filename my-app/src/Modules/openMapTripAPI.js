e5d67f3a5e4ff45739abb5828b47ee20a7006d4e
DrIronfist <reetsinha2006@gmail.com>
2022-07-16 00:29:54 -0500

    integration fixes

--- my-app/src/Modules/openMapTripAPI.js
+++ my-app/src/Modules/openMapTripAPI.js
@@ -1,8 +1,9 @@
 let apiKey = "5ae2e3f221c38a28845f05b6fb84e1655d2dfb8b81834c5283a5dfae"
-let databaseURL = "http://api.opentripmap.com/0.1/en/places?"
+let databaseURL = "http://api.opentripmap.com/0.1/en/places/autosuggest?"
 
 export async function autoSuggestPlace(name, radius, lon, lat, category, callback){
     let requestURL = databaseURL + "name=" + name + "&radius="+radius+"&lon="+lon+"&lat=" + lat + "&kind=" + category + "&format=json" + "&apikey=" + apiKey
+    console.log(requestURL)
     let response = await fetch(requestURL)
     let data = await response.json()
     if(typeof(callback)=="function"){
