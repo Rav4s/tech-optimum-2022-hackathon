5380c2810a9ace6e7dca299383e47d99722d4ef3
JadonLee8 <jadonlee888@gmail.com>
2022-07-16 12:41:45 -0500

    folder

---
+++ my-app/src/components/Nav/index.js
@@ -0,0 +1,9 @@
+import {Link, NavLink} from 'react-router-dom';
+import './index.css';
+function NavBar() {
+    return (
+        <div>hello</div>
+    )
+}
+
+export default NavBar;
--- my-app/src/components/index.js
+++
@@ -1,2 +0,0 @@
-import {Link, NavLink} from 'react-router-dom'
-import './index.scss'
