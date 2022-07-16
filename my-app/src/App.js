7f592105139cdb1b19011c5b7f30638be6586399
DrIronfist <reetsinha2006@gmail.com>
2022-07-16 11:16:48 -0500

    merge fixes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import { app, analytics, auth } from './firebase';
-import {getDataByName, getCoordinates, getCurrency, getLanguages, getCapitalCity, hasOceanAccess, getRegion, getFlagSVG} from "./modules/countryData.js";
+import {getDataByName, getCoordinates, getCurrency, getLanguages, getCapitalCity, hasOceanAccess, getRegion, getFlagSVG} from "./Modules/countryData.js";
 import Dashboard from './Dashboard';
 import Router from './Router';
 
