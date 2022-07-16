6448d2cd8ee3070d4f61f2f671bcd19b75033e54
DrIronfist <78988021+DrIronfist@users.noreply.github.com>
2022-07-15 21:46:21 -0500

    host react app on firebase (#4)
    make sure to npm install

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDh5UwDsmEjRnLNFIyEdXWB7LTxzHnL84o",
+  authDomain: "techoptimumhackathon.firebaseapp.com",
+  projectId: "techoptimumhackathon",
+  storageBucket: "techoptimumhackathon.appspot.com",
+  messagingSenderId: "257690996988",
+  appId: "1:257690996988:web:3996a70a525c1daaf30779",
+  measurementId: "G-HW1VSFN6LS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
   return (
