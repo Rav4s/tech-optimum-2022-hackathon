538285ce92c4c5a22a4286d31cd2a53b18c72626
JadonLee8 <jadonlee888@gmail.com>
2022-07-16 18:44:09 -0500

    creating input box for country info page

--- my-app/src/components/CountryInfoPage.js
+++ my-app/src/components/CountryInfoPage.js
@@ -1,9 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 function CountryInfoPage(){
     return (
         <>
-            hello
+            <div className="input area">
+            <input
+                type="text"
+                className="login__textBox"
+                //value={email}
+                //onChange={(e) => setEmail(e.target.value)}
+                placeholder="Country Name"
+            />
+            </div>
         </>
     )
 }
+
+export default CountryInfoPage
