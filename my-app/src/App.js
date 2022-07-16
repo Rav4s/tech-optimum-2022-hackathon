2292937356dd4bc5d003042f7fa45b101e69ed08
JadonLee8 <jadonlee888@gmail.com>
2022-07-16 14:45:42 -0500

    progress

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import { app, analytics, auth } from './firebase';
 import {getDataByName, getCoordinates, getCurrency, getLanguages, getCapitalCity, hasOceanAccess, getRegion, getFlagSVG} from "./Modules/countryData.js";
 import Dashboard from './Dashboard';
 import Router from './Router';
+import NavBar from './components/Nav';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       
     </div>
     <div className="main">
+      <NavBar />
       <Router>
       </Router>
     </div>
--- my-app/src/components/Nav/index.js
+++ my-app/src/components/Nav/index.js
@@ -1,8 +1,50 @@
 import {Link, NavLink} from 'react-router-dom';
 import './index.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 function NavBar() {
     return (
-        <div>hello</div>
+        <div className='Navbar'>
+            <Navbar.Brand href="#home" className='brand'>Nexus</Navbar.Brand>
+            <div className='logo'>
+                <img src='./NavLogo.png' alt='logo' />
+            </div>
+            <div className='NavLinks'>
+                <Link to='../about'>About</Link>
+            </div>
+            <div className='login-info'>
+                
+            </div>
+        </div>
+        //bootstrap not working, going to write own navbar
+        // <Container className='Navbar-container'>
+        // <Navbar bg="light" className='Navbar'>
+        // <Container>
+        //     <Navbar.Brand href="#home" className='brand'>React-Bootstrap</Navbar.Brand>
+        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        //     <Navbar.Collapse id="basic-navbar-nav">
+        //     <Nav className="me-auto">
+        //         <Nav.Link href="#home">Home</Nav.Link>
+        //         <Nav.Link href="#link">Link</Nav.Link>
+        //         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        //         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        //         <NavDropdown.Item href="#action/3.2">
+        //             Another action
+        //         </NavDropdown.Item>
+        //         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        //         <NavDropdown.Divider />
+        //         <NavDropdown.Item href="#action/3.4">
+        //             Separated link
+        //         </NavDropdown.Item>
+        //         </NavDropdown>
+        //     </Nav>
+        //     </Navbar.Collapse>
+        // </Container>
+        // </Navbar>
+        // </Container>
     )
 }
 
