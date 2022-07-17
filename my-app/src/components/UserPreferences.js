60c438285d949ce0f7912bb8c17b7c54d7babbab
DrIronfist <reetsinha2006@gmail.com>
2022-07-16 23:21:22 -0500

    firestore update

--- my-app/src/components/UserPreferences.js
+++ my-app/src/components/UserPreferences.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { updateUserPreferences } from "./firebase"
 const budgetTypes = {
     '$':{
         val: "low",
@@ -23,12 +24,32 @@ const travelTypes = {
     }
 }
 
+const categoryTypes = {
+    'Natural':{
+        val:'natural'
+    },
+    'Architecture':{
+        val:'architecture'
+    },
+    'Cultural':{
+        val:'cultural'
+    },
+    'Historic':{
+        val:'historic'
+    }
+}
+
 export default function UserPreferences(){
     const [travelType,setTravelType] = React.useState("air")
     const [budget, setBudget] = React.useState("medium")
+    const [categories, setCategories] = React.useState([])
 
     function handleSubmit(e){
-        console.log([travelType, budget])
+        updateUserPreferences({
+            travelType:travelType,
+            budgetType: budget,
+            categoryTypes: categories
+        })
         e.preventDefault();
     }
 
@@ -42,8 +63,6 @@ export default function UserPreferences(){
                         Budget Type:
                         <select value={budget} onChange={(e)=>{setBudget(e.target.value)}}>
                             {Object.keys(budgetTypes).map((i)=>{
-                                console.log(i)
-                                console.log(budgetTypes[i].val);
                                 return <option key={i} value = {budgetTypes[i].val}>{i}</option>
                             })}
                         </select>
@@ -54,11 +73,38 @@ export default function UserPreferences(){
                         Travel Type:
                         <select value={travelType} onChange={(e)=>{setTravelType(e.target.value)}}>
                             {Object.keys(travelTypes).map((i)=>{
-                                console.log(i)
-                                console.log(travelTypes[i].val);
                                 return <option key={i} value = {travelTypes[i].val}>{i}</option>
                             })}
                         </select>
+                    </label>
+                </div>
+                <div>
+                    <label for="categories">
+                        Categories Select:
+                        {Object.keys(categoryTypes).map((i)=>{
+                            return( 
+                            <div>
+                                {i}
+                                <input type="checkbox" id={i} name={i} value={categoryTypes[i].val} onChange={(e)=>{
+                                    if(e.target.checked){
+                                        setCategories(old=>{
+                                            return [...old,e.target.value]
+                                        })
+                                    } else{
+                                        setCategories(categories.filter(data => data!=e.target.value))
+                                    }
+                                }}/>
+                            </div>)
+                        })}
+                        {/* <select name="categories" id = "categories" onChange={(e)=>{
+                            console.log(e)
+                            setCategories(e.target.value)}} multiple>
+                            {Object.keys(categoryTypes).map((i)=>{
+                                console.log(i)
+                                console.log(categoryTypes[i].val);
+                                return <option key={i} value = {categoryTypes[i].val}>{i}</option>
+                            })}
+                        </select> */}
                     </label>
                 </div>
                 <input type="submit" value="Submit" />
