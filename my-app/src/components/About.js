7547631fc2b81918e2eb064d05bd096f2e41cf7c
JadonLee8 <jadonlee888@gmail.com>
2022-07-17 10:11:36 -0500

    added link to github in about

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -11,7 +11,8 @@ function Account() {
                 <p>Nexus Travel is a <b>data-driven</b> travel application which fosters greater connection throughout our <b>global community</b>. Nexus provides a <b>simple filter system    </b> that predicts travel ideas based on each user's preferences. At the center of our vision is our core value of <b>open-mindedness</b>, allowing users to <b>expand</b> their mindset of global cultures.</p>
             </div>
             <p>Built with &#10084;&#65039; by Ravi Shah, Reet Sinha, Jadon Lee, Alex Bui, and Samarth Bikki for the Tech Optimum Hackathon 2022</p>
-            <p className="citations">A huge thanks to our APIs! <a href="https://opentripmap.io/catalog" target="_blank">https://opentripmap.io/catalog</a> - <a href="https://restcountries.com/" target="_blank">https://restcountries.com/</a></p>
+            <p className="citations">A huge thanks to our APIs! <a href="https://opentripmap.io/catalog" target="_blank" rel="noreferrer noopener">https://opentripmap.io/catalog</a> - <a href="https://restcountries.com/" target="_blank" rel="noreferrer noopener">https://restcountries.com/</a></p>
+            <p>The website's Github: <a href="https://github.com/Rav4s/tech-optimum-2022-hackathon" target="_blank" rel="noreferrer noopener">https://github.com/Rav4s/tech-optimum-2022-hackathon</a></p>
         </div>
     );
 }
